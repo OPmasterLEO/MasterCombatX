87d258e7ed2459b8159f2c0d7639b2f06cd8c511
OPmasterLEO <kalenikleo@gmail.com>
2025-06-21 12:13:56 +0200

    Futher optimization

--- src/main/java/net/opmasterleo/combat/listener/EntityDamageByEntityListener.java
+++ src/main/java/net/opmasterleo/combat/listener/EntityDamageByEntityListener.java
@@ -25,70 +25,85 @@ public final class EntityDamageByEntityListener implements Listener {
         if (combat.getWorldGuardUtil() != null && combat.getWorldGuardUtil().isPvpDenied(player)) return;
 
         Entity damager = event.getDamager();
+        boolean selfCombat = combat.getConfig().getBoolean("self-combat", false);
+        boolean linkProjectiles = combat.getConfig().getBoolean("link-projectiles", true);
+        boolean linkEndCrystals = combat.getConfig().getBoolean("link-end-crystals", true);
+        boolean linkPets = combat.getConfig().getBoolean("link-pets", true);
+        boolean linkFishingRod = combat.getConfig().getBoolean("link-fishing-rod", true);
+        boolean linkTnt = combat.getConfig().getBoolean("link-tnt", true);
+        Set<String> ignoredProjectiles = combat.getIgnoredProjectiles();
 
         if (damager instanceof Player damagerP) {
             if (damagerP.getUniqueId().equals(player.getUniqueId())) {
-                if (combat.getConfig().getBoolean("self-combat", false)) {
-                    combat.setCombat(player, player);
+                if (selfCombat) {
+                    if (!combat.isInCombat(player))
+                        combat.setCombat(player, player);
                 }
                 return;
             }
-            combat.setCombat(player, damagerP);
-            combat.setCombat(damagerP, player);
+            if (!combat.isInCombat(player) || combat.getCombatOpponent(player) == null || !combat.getCombatOpponent(player).getUniqueId().equals(damagerP.getUniqueId())) {
+                combat.setCombat(player, damagerP);
+                combat.setCombat(damagerP, player);
+            }
         }
 
-        if (damager instanceof Projectile projectile) {
-            boolean linkProjectiles = combat.getConfig().getBoolean("link-projectiles", true);
-            boolean selfCombat = combat.getConfig().getBoolean("self-combat", false);
-            if (!linkProjectiles) return;
-
+        if (damager instanceof Projectile projectile && linkProjectiles) {
             if (projectile.getShooter() instanceof Player shooter) {
                 if (shooter.getUniqueId().equals(player.getUniqueId())) {
-                    if (selfCombat) {
+                    if (selfCombat && !combat.isInCombat(player)) {
                         combat.setCombat(player, player);
                     }
                 } else {
-                    Set<String> ignoredProjectiles = combat.getIgnoredProjectiles();
-                    if (ignoredProjectiles.contains(projectile.getType().name().toUpperCase())) return;
+                    if (!ignoredProjectiles.contains(projectile.getType().name().toUpperCase())) {
+                        if (!combat.isInCombat(player) || combat.getCombatOpponent(player) == null || !combat.getCombatOpponent(player).getUniqueId().equals(shooter.getUniqueId())) {
+                            combat.setCombat(player, shooter);
+                            combat.setCombat(shooter, player);
+                        }
+                    }
+                }
+            }
+        }
 
+        if (linkEndCrystals && damager.getType() == EntityType.END_CRYSTAL) {
+            Player placer = combat.getCrystalManager().getPlacer(damager);
+            if (placer != null) {
+                if (placer.getUniqueId().equals(player.getUniqueId()) && !selfCombat) {
+                    return;
+                }
+                if (!combat.isInCombat(player) || combat.getCombatOpponent(player) == null || !combat.getCombatOpponent(player).getUniqueId().equals(placer.getUniqueId())) {
+                    combat.setCombat(player, placer);
+                    combat.setCombat(placer, player);
+                }
+            }
+        }
+
+        if (linkPets && damager instanceof Tameable tameable) {
+            if (tameable.getOwner() instanceof Player owner) {
+                if (owner.getUniqueId().equals(player.getUniqueId())) return;
+                if (!combat.isInCombat(player) || combat.getCombatOpponent(player) == null || !combat.getCombatOpponent(player).getUniqueId().equals(owner.getUniqueId())) {
+                    combat.setCombat(player, owner);
+                    combat.setCombat(owner, player);
+                }
+            }
+        }
+
+        if (linkFishingRod && damager instanceof FishHook fishHook) {
+            if (fishHook.getShooter() instanceof Player shooter) {
+                if (shooter.getUniqueId().equals(player.getUniqueId())) return;
+                if (!combat.isInCombat(player) || combat.getCombatOpponent(player) == null || !combat.getCombatOpponent(player).getUniqueId().equals(shooter.getUniqueId())) {
                     combat.setCombat(player, shooter);
                     combat.setCombat(shooter, player);
                 }
             }
         }
 
-        if (combat.getConfig().getBoolean("link-end-crystals", true) && damager.getType() == EntityType.END_CRYSTAL) {
-            Player placer = combat.getCrystalManager().getPlacer(damager);
-            if (placer != null) {
-                if (placer.getUniqueId().equals(player.getUniqueId()) && !combat.getConfig().getBoolean("self-combat", false)) {
-                    return;
-                }
-                combat.setCombat(player, placer);
-                combat.setCombat(placer, player);
-            }
-        }
-
-        if (combat.getConfig().getBoolean("link-pets", true) && damager instanceof Tameable tameable) {
-            if (tameable.getOwner() instanceof Player owner) {
-                if (owner.getUniqueId().equals(player.getUniqueId())) return;
-                combat.setCombat(player, owner);
-                combat.setCombat(owner, player);
-            }
-        }
-
-        if (combat.getConfig().getBoolean("link-fishing-rod", true) && damager instanceof FishHook fishHook) {
-            if (fishHook.getShooter() instanceof Player shooter) {
-                if (shooter.getUniqueId().equals(player.getUniqueId())) return;
-                combat.setCombat(player, shooter);
-                combat.setCombat(shooter, player);
-            }
-        }
-
-        if (combat.getConfig().getBoolean("link-tnt", true) && damager instanceof TNTPrimed tnt) {
+        if (linkTnt && damager instanceof TNTPrimed tnt) {
             if (tnt.getSource() instanceof Player source) {
                 if (source.getUniqueId().equals(player.getUniqueId())) return;
-                combat.setCombat(player, source);
-                combat.setCombat(source, player);
+                if (!combat.isInCombat(player) || combat.getCombatOpponent(player) == null || !combat.getCombatOpponent(player).getUniqueId().equals(source.getUniqueId())) {
+                    combat.setCombat(player, source);
+                    combat.setCombat(source, player);
+                }
             }
         }
     }
